ae8144dbe88eebaec887ea0e329914add58486e2
Maciej Karaśkiewicz <maciej.karaskiewicz@clarite.pl>
2024-11-06 19:46:31 +0100

    zarys struktury

---
+++ hopfield/base.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from .rules import LearningRule
+
+
+class Hopfield:
+    def __init__(self, input_shape: tuple[int, int], learning_rule: LearningRule) -> None:
+        pass
+
+    def train(synchronous: bool) -> None:
+        pass
---
+++ hopfield/rules.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+
+class LearningRule(ABC):
+    pass
+
+
+class HebbRule(LearningRule):
+    pass
+
+
+class OjiRule(LearningRule):
+    pass
