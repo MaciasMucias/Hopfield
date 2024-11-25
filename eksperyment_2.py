814a9645ea57ff7ba52e478ff0d97069f243c500
Hubert Malinowski <130050838+HubertMalinowski@users.noreply.github.com>
2024-11-25 11:07:50 +0100

    Create eksperyment_2.py

---
+++ eksperyment_2.py
@@ -0,0 +1,39 @@
+from typing import cast
+import numpy as np
+
+pattern = np.genfromtxt("/content/new_patterns.csv", delimiter=",")
+
+model = Hopfield((100, 100), HebbianRule(), 0)
+model.train(pattern)
+
+# Uszkodzenia 25%
+damaged_pattern = pattern[2]
+damaged_pattern[:2500] = -1
+
+_, history = model.predict(damaged_pattern, "synchronous", save_history=True)
+history = cast(list[np.ndarray], history)
+first_last_frame(history, (100, 100))
+
+#Uszkodzenia 50%
+damaged_pattern = pattern[2]
+damaged_pattern[:5000] = -1
+
+_, history = model.predict(damaged_pattern, "synchronous", save_history=True)
+history = cast(list[np.ndarray], history)
+first_last_frame(history, (100, 100))
+
+#Uszkodzenia 75%
+damaged_pattern = pattern[2]
+damaged_pattern[:7500] = -1
+
+_, history = model.predict(damaged_pattern, "synchronous", save_history=True)
+history = cast(list[np.ndarray], history)
+first_last_frame(history, (100, 100))
+
+#Uszkodzenia 90%
+damaged_pattern = pattern[2]
+damaged_pattern[:9000] = -1
+
+_, history = model.predict(damaged_pattern, "synchronous", save_history=True)
+history = cast(list[np.ndarray], history)
+first_last_frame(history, (100, 100))
